1bc44f75d587cef5697c74d9b7a93af97f9c3982
sultanien <sultanertin@gmail.com>
2022-12-05 18:36:09 +0100

    'implementations are done'

--- homework/HackYourTempeture/__test__/app.test.js
+++ homework/HackYourTempeture/__test__/app.test.js
@@ -1,7 +1,7 @@
 /** @format */
 
 import app from "../app.js";
-import { KelvinToCelsius, setImplementation } from "../app.js";
+import { KelvinToCelsius} from "../app.js";
 import supertest from "supertest";
 
 const request = supertest(app);
